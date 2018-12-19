44a5a118514656234f55543e75f24486ff816f50
Mike Kaperys <mike@kaperys.io>
2018-12-19 08:49:27 +0000

    added --all flag

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
-// Copyright (c) 2018, Mike Kaperys <mike@kaperys.io>
-// See LICENSE for licensing information
+// Copyright (c) 2018, Mike Kaperys <mike@kaperys.io>.
+// See LICENSE for licensing information.
 package main
 
 import (
@@ -14,21 +14,25 @@ import (
 	"github.com/aws/aws-sdk-go/service/ec2"
 )
 
-// version stores the eip version. This is overwritten at compile-time using ldflags.
-var version = "0.1.1"
+var (
+	// version stores the eip version. This value is overwritten at compile-time using ldflags.
+	version = "0.2.0"
 
-// version stores the eip build commit hash. This is overwritten at compile-time using ldflags.
-var build = "dev"
+	// version stores the eip build commit hash. This value is overwritten at compile-time using ldflags.
+	build = "dev"
+)
 
 const (
+	_ = iota
+
 	// The exitInvalidFilter exit code is returned when a provided `--filter` flag is malformed or invalid.
-	exitInvalidFilter = 1 << iota
+	exitInvalidFilter
 
 	// The exitBadAWSSession exit code is returned when an AWS SDK session cannot be initialised.
 	exitBadAWSSession
 
 	// The exitCannotDescribeInstances exit code is returned when there was an error describing instances.
-	// This is usually caused by missing environment variables (like AWS_REGION) or permissions.
+	// This is usually caused by a missing required environment variable (like AWS_REGION) or bad permissions.
 	exitCannotDescribeInstances
 
 	// The exitNoInstancesFound exit code is returned when no EC2 instances are found using the given filters.
@@ -46,12 +50,13 @@ or --private flag must be provided. eip makes no assumptions about output.
 	--filter   filters results using the provided key-value pair (FilterName=FilterValue)
 	--public   show the instance public IP address
 	--private  show the instance private IP address
+	--all      show private IP addresses associated with all instance network interfaces (requires the --private flag)
 
-Exit Codes:
+Exit codes:
 	%d   an invalid --filter flag was provided
 	%d   an AWS session could not be initialised
 	%d   EC2 instances could not be described
-	%d   no EC2 instances were found\n
+	%d   no EC2 instances were found
 `, exitInvalidFilter, exitBadAWSSession, exitCannotDescribeInstances, exitNoInstancesFound)
 	}
 
@@ -60,12 +65,14 @@ Exit Codes:
 		filterFlags filterFlags
 		publicFlag  bool
 		privateFlag bool
+		allFlag     bool
 	)
 
 	flag.BoolVar(&versionFlag, "version", false, "show the eip version information")
 	flag.Var(&filterFlags, "filter", "filter used to retrieve addresses")
 	flag.BoolVar(&publicFlag, "public", false, "show instance public ip address")
 	flag.BoolVar(&privateFlag, "private", false, "show the instact private ip address")
+	flag.BoolVar(&allFlag, "all", false, "return all private IP addresses (from all network interfaces)")
 
 	flag.Parse()
 
@@ -74,11 +81,18 @@ Exit Codes:
 		return
 	}
 
-	resolve(parseFilters(filterFlags), publicFlag, privateFlag)
+	resolve(parseFilters(filterFlags), publicFlag, privateFlag, allFlag)
 }
 
-// resolve parses the given flag data and uses the AWS SDK to describe EC2 instances.
-func resolve(ff []*ec2.Filter, pub, priv bool) {
+// resolve creates an AWS SDK session and attempts to describe EC2 instances using the given
+// flag data.
+//
+// Results are filtered using the provided slice of *ec2.Filters. For the matching results the
+// following logic is applied:
+//  If `pub` is true the associated public IP address is returned.
+//  If `pri` is true the associated private IP address is returned.
+//  If `pri` and `all` are both true, the private IP addresses associated with all instance network interfaces are returned.
+func resolve(ff []*ec2.Filter, pub, pri, all bool) {
 	insIn := &ec2.DescribeInstancesInput{Filters: ff}
 
 	s, err := session.NewSession()
@@ -100,20 +114,31 @@ func resolve(ff []*ec2.Filter, pub, priv bool) {
 
 	for _, r := range result.Reservations {
 		for _, inst := range r.Instances {
-			if pub && inst.PublicIpAddress != nil {
-				fmt.Println(*inst.PublicIpAddress)
+			if all && pri && len(inst.NetworkInterfaces) != 0 {
+				for _, nic := range inst.NetworkInterfaces {
+					fmt.Println(*nic.PrivateIpAddress)
+				}
+
+				continue
 			}
 
-			if priv && inst.PrivateIpAddress != nil {
+			if pri && inst.PrivateIpAddress != nil {
 				fmt.Println(*inst.PrivateIpAddress)
+				continue
+			}
+
+			if pub && inst.PublicIpAddress != nil {
+				fmt.Println(*inst.PublicIpAddress)
+				continue
 			}
 		}
 	}
 }
 
-// parseFilters attempt to parse the given --filter flags. Filter flags are provided
+// parseFilters attempts to parse the given --filter flags. Filter flags are provided
 // using the following syntax: `--filter tag:SystemGroup=api,app --filter tag:Name=my-ec2-instance`.
-// `--filter` supports flags provided by the AWS SDK:
+//
+// `--filter` supports all filters provided by the AWS Go SDK. See the following link:
 // https://github.com/datacratic/aws-sdk-go/blob/master/service/ec2/api.go#L9532-L9754
 func parseFilters(ff filterFlags) []*ec2.Filter {
 	var filters []*ec2.Filter
@@ -136,7 +161,7 @@ func parseFilters(ff filterFlags) []*ec2.Filter {
 	return filters
 }
 
-// filterFlags is a type used to represent the `--filter` flag, since there can be many.
+// filterFlags is a type used to represent the `--filter` flag, since many are supported.
 type filterFlags []string
 
 // String returns the string representation of the provided filter flags.
@@ -144,7 +169,7 @@ func (i *filterFlags) String() string {
 	return strings.Join(*i, ",")
 }
 
-// Set sets a new filter flag in the slice.
+// Set adds a new filter flag to the slice.
 func (i *filterFlags) Set(value string) error {
 	*i = append(*i, value)
 	return nil
